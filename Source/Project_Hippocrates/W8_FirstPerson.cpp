a90ad98ca7e0976c7d31cefeb599463fed9e8e8f
Alessandro_Cinque <alessandro5cinque@gmail.com>
2020-02-18 17:20:02 +0000

    Revert "new soruces"
    This reverts commit 5bc4945d5ef4089b8a93bf75b3720f873d5eb86d.

--- Source/Project_Hippocrates/W8_FirstPerson.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "W8_FirstPerson.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, W8_FirstPerson, "W8_FirstPerson" );
